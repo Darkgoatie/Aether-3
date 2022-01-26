cc639b1933e994a1e0fa090d2e8eb82b391fb9ef
Darkgoatie <81323822+Darkgoatie@users.noreply.github.com>
2022-01-26 21:28:20 +0300

    Create command handler

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ const config = require("./config.json");
 
 const client = new Client({ intents: Intents.FLAGS.GUILDS });
 
+const commands = [];
+files.forEach((file) => {
+    if(file.endsWith(".js")){
+        commands.push(require(`./Commands/${file}`));
+    };
+});
+
+client.on("interactionCreate", (interaction) => {
+    if(interaction.isCommand()) {
+        const cmd = commands.find(interaction.commandName);
+        cmd.onInteraction(interaction);
+    }
+});
+
 client.login(config.bot.token);
