591210334844b27990608ec4ef05965e4de08ae2
Darkgoatie <81323822+Darkgoatie@users.noreply.github.com>
2022-01-27 11:54:26 +0300

    Fix help menu command, fully working. Needs to be filled with content

--- Commands/help.js
+++ Commands/help.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
+const { MessageEmbed, MessageButton, MessageActionRow, Client, Base, Message, GuildEmojiRoleManager } = require("discord.js");
 const wait = require("util").promisify(setTimeout); 
+const config = require("../config.json");
 
 const name = "help";
 const description = "Shows the help menu";
@@ -20,37 +21,44 @@ const onInteraction = async (int) => {
     const prevPageBtn = new MessageButton()
         .setCustomId(`helpPrevPageBtn-${int.user.id}`)
         .setLabel("previous page")
-        .setStyle("PRIMARY")
+        .setStyle("SECONDARY")
         .setEmoji("935822573827141632");
 
     const row = new MessageActionRow()
-        .addComponents(nextPageBtn , prevPageBtn);
+        .addComponents(prevPageBtn , nextPageBtn);
 
     let pageNo = 1;
+
+    function createBaseEmbed()
+    {
+        return new MessageEmbed()
+            .setAuthor({ name: int.user.tag, iconURL: int.user.avatarURL() })
+            .setTitle("Help menu")
+            .setDescription("[Invite Aether!](https://aether.vercel.app/invite) / [Join the support server!](https://aether.vercel.app/support)")
+            .setTimestamp(new Date())
+            .setColor("RANDOM");
+    }
 
     const pages = [
         {
             no: 1,
-            content: "Page 1..."
+            content: createBaseEmbed()
+                .setFooter({ iconURL: config.bot.iconURL, text: "Page 1/3 | Thank you for using Aether!" }),
         },
         {
             no: 2,
-            content: "Page 2..."
+            content: createBaseEmbed()
+                .setFooter({ iconURL: config.bot.iconURL, text: "Page 2/3 | Thank you for using Aether!" }),
         },
         {
             no: 3,
-            content: "Page 3..."
+            content: createBaseEmbed()
+                .setFooter({ iconURL: config.bot.iconURL, text: "Page 3/3 | Thank you for using Aether!" }),
         }
-    ]
+    ];
 
-    setTimeout(() => {
-        prevPageBtn.setDisabled();
-        nextPageBtn.setDisabled();
-
-    }, 15e3);
-
-    await int.channel.send({ content: pages.find(page => page.no === 1), components: [ row ] });
-
+    const sentMessage = await int.channel.send({ components: [ row ], embeds: [ pages.find(page => page.no == pageNo).content ] });
+    int.reply({ content: "A help menu was sent!", ephemeral: true });
     const filter = i => i.customId.startsWith('help') && i.customId.endsWith(int.user.id);
     const collector = int.channel.createMessageComponentCollector({ filter, time: 15e3 });
 
@@ -60,25 +68,35 @@ const onInteraction = async (int) => {
             if (pageNo === 1) 
             {
                 i.channel.send({ content: "There are no previous pages!"});
+                i.update({ components: [ row ], embeds: [ pages.find(page => page.no == pageNo).content ] });
             }
             else 
             {            
                 pageNo--;
-                i.update({ content: pages.find(page => page.no == pageNo).content });
+                i.update({ components: [ row ], embeds: [ pages.find(page => page.no == pageNo).content ] });
             }
         }
         else if (i.customId.startsWith("helpNext"))
         {
             if(pageNo === 3) {
                 i.channel.send({ content: "There are no next pages!" });
+                i.update({ components: [ row ], embeds: [ pages.find(page => page.no == pageNo).content ] });
             }
             else
             {
                 pageNo++;
-                i.update({ content: pages.find(page => page.no == pageNo).content });
+                i.update({ components: [ row ], embeds: [ pages.find(page => page.no == pageNo).content ] });
             }
         }
     });
+
+    collector.on("end", async (collected) => {
+        row.components.forEach((comp) => {
+            comp.setDisabled();
+        });
+        sentMessage.edit({ components: [ row ], embeds: [ pages.find(page => page.no == pageNo).content ] });
+    })
+
 }
 
 module.exports = {
