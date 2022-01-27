9f4b1d46992cee2fbae31c4d0f53486f3565b491
Darkgoatie <81323822+Darkgoatie@users.noreply.github.com>
2022-01-27 11:10:30 +0300

    Put all code in single function

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-const { Client, Intents } = require("discord.js");
-const { readdir } = require("fs");
-const path = require("path")
-const config = require("./config.json");
+async function main () {
+    const { Client, Intents } = require("discord.js");
+    const { readdir } = require("fs");
+    const path = require("path")
+    const config = require("./config.json");
 
-const client = new Client({ intents: Intents.FLAGS.GUILDS });
-const directoryPath = path.join(__dirname, 'Commands');
+    const client = new Client({ intents: Intents.FLAGS.GUILDS });
+    const directoryPath = path.join(__dirname, 'Commands');
 
-readdir(directoryPath, (err, files) => {
-    if (err) {
-        return console.log("Unable to scan directory: " + err);
-    }
+    readdir(directoryPath, (err, files) => {
+        if (err) {
+            return console.log("Unable to scan directory: " + err);
+        }
+        const commands = [];    
+        files.forEach((file) => {
+            if(file.endsWith(".js")){
+                commands.push(require(`./Commands/${file}`));
+            };
+        });
 
-    const commands = [];
-    files.forEach((file) => {
-        if(file.endsWith(".js")){
-            commands.push(require(`./Commands/${file}`));
-        };
-    });
+        client.on("interactionCreate", (interaction) => {
+            if(interaction.isCommand()) {
+                const cmd = commands.find(oksd => oksd.name === interaction.commandName);
+                cmd.onInteraction(interaction);
+            }
+        });
 
-})
+        client.once("ready", () => {
+            console.log(`Client ready at ${client.guilds.cache.size} servers.`);
+        });
 
+        client.login(config.bot.token);
 
-client.on("interactionCreate", (interaction) => {
-    if(interaction.isCommand()) {
-        const cmd = commands.find(interaction.commandName);
-        cmd.onInteraction(interaction);
-    }
-});
+    })
+}
 
-client.once("ready", () => {
-    console.log(`Client ready at ${client.guilds.cache.size} servers.`);
-});
-
-client.login(config.bot.token);
+main();
