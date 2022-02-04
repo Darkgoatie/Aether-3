f3c30c7bd4578103768dea4f31a7a8019e8e6981
Darkgoatie <81323822+Darkgoatie@users.noreply.github.com>
2022-02-04 15:54:33 +0300

    Change express port to 8080

--- src/express.js
+++ src/express.js
@@ -1,24 +1,24 @@
 async function runExp({ voteCallback }) {
-    require("dotenv").config();
-    const express = require("express");
-    const app = express();
-    const Topgg = require("@top-gg/sdk");
+  require("dotenv").config();
+  const express = require("express");
+  const app = express();
+  const Topgg = require("@top-gg/sdk");
 
-    app.use(express.static("public"));
-    const Hook = new Topgg.Webhook(process.env.TopggAuth);
-    // define the first route
-    app.post("/votes", Hook.listener(voteCallback));
+  app.use(express.static("public"));
+  const Hook = new Topgg.Webhook(process.env.TopggAuth);
+  // define the first route
+  app.post("/votes", Hook.listener(voteCallback));
 
-    app.get("/", async (req, res) => {
-        res.send("<h1>This is the Aether api! Please don't interact with unless you are given permission to!</h1>")
-    })
-
-    // start the server listening for requests
-    app.listen(80, 
-    	() => console.log("Server is running...")
+  app.get("/", async (req, res) => {
+    res.send(
+      "<h1>This is the Aether api! Please don't interact with unless you are given permission to!</h1>"
     );
+  });
+
+  // start the server listening for requests
+  app.listen(8080, () => console.log("Server is running..."));
 }
 
 module.exports = {
-    runExp
+  runExp,
 };
