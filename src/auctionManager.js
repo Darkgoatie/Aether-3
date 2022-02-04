3f4200c06e173c717068f6cd4a519d5efb4212b8
Darkgoatie <81323822+Darkgoatie@users.noreply.github.com>
2022-02-04 21:39:47 +0300

    Add autoEndSettings to auction Schema

--- src/auctionManager.js
+++ src/auctionManager.js
@@ -1,15 +1,19 @@
 const { mongoose } = require("./mongoose");
 
 const aucSchema = new mongoose.Schema({
-    channelId: String,
-    guildId: String,
-    messageId: String,
-    price: Number,
-    startedAt: Number,
-    winner: String,
-    item: String,
-    hostedBy: String,
-    totalBids: Number
+  channelId: String,
+  guildId: String,
+  messageId: String,
+  price: Number,
+  startedAt: Number,
+  winner: String,
+  item: String,
+  hostedBy: String,
+  totalBids: Number,
+  autoEndSettings: {
+    autoEnd: Boolean,
+    endAt: Number,
+  },
 });
 
 const aucModel = mongoose.model("auctions", aucSchema);
